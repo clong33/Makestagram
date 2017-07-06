f16f3f1023d9d7a554587501bd0472af2cee9038
Cameron Long <cameron33long@gmail.com>
2017-07-06 15:45:02 -0400

    Created service layer
    user can now move to main storyboard after creating username

--- Makestagram/Controllers/CreateUsernameViewController.swift
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -23,30 +23,22 @@ class CreateUsernameViewController: UIViewController {
     
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        // 1
         guard let firUser = Auth.auth().currentUser,
             let username = usernameTextField.text,
             !username.isEmpty else { return }
         
-        // 2
-        let userAttrs = ["username": username]
-        
-        // 3
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-        
-        // 4
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                return
-            }
+        UserService.create(firUser, username: username) { (user) in
+            guard let _ = user else { return }
             
-            // 5
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-                
-                // handle newly created user here
-            })
+            //create new instance of main storyboard
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            
+            //check that storyboard has an initial view controller
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+                //Get reference to the current window and set the rootViewController to the initial view controller
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
         }
     }
     
